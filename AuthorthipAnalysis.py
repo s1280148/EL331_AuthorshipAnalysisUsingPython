4739e47b64df77d4bcc731848a09ef13c4848d39
s1280148 <s1280148@u-aizu.ac.jp>
2022-05-29 23:24:10 +0900

    Create analysis phase.

--- AuthorthipAnalysis.py
+++ AuthorthipAnalysis.py
@@ -4,6 +4,7 @@ import os
 import collections
 import random
 import re
+import math
 from dotenv import load_dotenv
 import tweepy
 import nltk
@@ -51,6 +52,8 @@ class AuthorshipVerifier:
     ngram_count_by_author = dict()
     each_word_occurrence_rate_by_author = dict()
 
+    ngram_numbers = [1, 2, 3]
+
     def __init__(self, client, user_name_1, user_name_2):
         self.client = client
         self.user_name_1 = user_name_1
@@ -69,7 +72,7 @@ class AuthorshipVerifier:
             user_id = self.client.get_user(username = user_name).data.id
 
             pagination_token = None
-            for i in range(20):
+            for i in range(100):
                 tweets = self.client.get_users_tweets(id = user_id,
                                                  max_results = 100,
                                                  exclude = "retweets",
@@ -79,7 +82,10 @@ class AuthorshipVerifier:
                     remove_url_text = re.sub(r"\S*https?:\S*", "", tweet_data["text"])
                     self.tweet_texts_by_author[user_name].append(remove_url_text)
 
-                pagination_token = tweets.meta["next_token"]
+                try:
+                    pagination_token = tweets.meta["next_token"]
+                except KeyError:
+                    break
 
     def divide_questioned_texts_and_known_texts(self):
         for user_name in [self.user_name_1, self.user_name_2]:
@@ -95,11 +101,10 @@ class AuthorshipVerifier:
             self.questioned_texts_by_author[user_name] = questioned_texts
 
     def create_ngram_count_by_author(self):
-        ngram_numbers = [1, 2, 3]
         for user_name in [self.user_name_1, self.user_name_2]:
             self.ngram_count_by_author[user_name] = dict()
 
-            for n in ngram_numbers:
+            for n in self.ngram_numbers:
                 self.ngram_count_by_author[user_name][n] = collections.defaultdict(lambda: int())
 
                 for known_text in self.known_texts_by_author[user_name]:
@@ -117,6 +122,41 @@ class AuthorshipVerifier:
                 word_occurrence = word_count / word_count_sum
                 self.each_word_occurrence_rate_by_author[user_name][word] = word_occurrence
 
+    def analysis(self):
+        total_questioned_text_cnt = sum(len(questioned_texts) for questioned_texts in self.questioned_texts_by_author.values())
+        correct_author_cnt = 0
+
+        for question_user_name in [self.user_name_1, self.user_name_2]:
+            for questioned_text in self.questioned_texts_by_author[question_user_name]:
+                score_dict = collections.defaultdict(lambda: int())
+                tokenized_text = get_tokenized_text(questioned_text)
+                for n in self.ngram_numbers:
+                    for ngram in nltk.ngrams(tokenized_text, n):
+                        for known_user_name in [self.user_name_1, self.user_name_2]:
+                            score_dict[known_user_name] += self.ngram_count_by_author[known_user_name][n][ngram] * math.pow(50, n - 1)
+
+                user_1_word_count = sum(self.ngram_count_by_author[self.user_name_1][1].values())
+                user_2_word_count = sum(self.ngram_count_by_author[self.user_name_2][1].values())
+
+                ratio = user_1_word_count / user_2_word_count
+                score_dict[self.user_name_1] /= ratio
+
+                for known_user_name in [self.user_name_1, self.user_name_2]:
+                    for word in nltk.ngrams(tokenized_text, 1):
+                        score_dict[known_user_name] += self.each_word_occurrence_rate_by_author[known_user_name][word] * 100000
+
+                result_user_name = str()
+                max_score = 0
+                for user_name, score in score_dict.items():
+                    if score > max_score:
+                        max_score = score
+                        result_user_name = user_name
+
+                if question_user_name == result_user_name:
+                    correct_author_cnt += 1
+
+        print(f'{correct_author_cnt} of {total_questioned_text_cnt} questioned texts were correctly determined.')
+
 
 # Main process of the program
 def main(args):
@@ -128,9 +168,22 @@ def main(args):
     user_name_1 = args[1]
     user_name_2 = args[2]
 
+    print(f'author 1: @{user_name_1}')
+    print(f'author 2: @{user_name_2}')
+
+    print()
+
     client = setup_twitter_api()
 
+    print("Creating verifier...")
+    print()
+
     authorshipVerifier = AuthorshipVerifier(client, user_name_1, user_name_2)
+
+    print("Analyzing...")
+    print()
+
+    authorshipVerifier.analysis()
 
 
 main(sys.argv)
